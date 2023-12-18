6aaaab366dcdeabbe844c0ae8399820529daa5af
jlewis10sixty <jlewis1060@gmail.com>
2023-12-18 21:45:08 +0000

    learning about lists

---
+++ listrev01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Learning or Reviewing about Lists | by Alta3 Research"""
+
+def main():
+    myemptylist = []
+
+    myemptylist.extend('192.168.102.55')
+
+    print(myemptylist)
+
+if __name__ == "__main__":
+    main()  
