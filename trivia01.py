45fd05bf24485fbc5950c09e0b22731d9f70ad0d
jlewis10sixty <jlewis1060@gmail.com>
2023-12-20 16:13:00 +0000

    Working with NASA APIs

--- trivia01.py
+++ trivia01.py
@@ -2,20 +2,19 @@
 
 import requests
 
-ITEMURL = "https://opentdb.com/api.php"
+itemurl = "https://opentdb.com/api.php"
 
 def main():
-    QUESTIONS = input('Enter the number of trivia questions you would like: ')
-    DIFFICULTY = input('Enter the difficulty level (easy, medium or hard): ')
-    items = requests.get(f"{ITEMURL}?amount={QUESTIONS}&difficulty={DIFFICULTY}")
-    items = items.json()
+    questions = input('Enter the number of trivia questions you would like: ')
+    difficulty = difficulty.lower(input('Enter the difficulty level (easy, medium or hard): '))
+    items = requests.get(f"{itemurl}?amount={questions}&difficulty={difficulty}").json()
     count = 1
     for q in items["results"]:
-        print("\n" + str(count) + ") " + q.get("question") + "\n")
-        print("Correct answer is: \n" + q.get("correct_answer") + "\n")
+        print("\n" + str(count) + ") " + html.unescape(q.get("question")) + "\n")
+        print("Correct answer is: \n" + html.unescape(q.get("correct_answer")) + "\n")
         count +=1
 # https://opentdb.com/api.php?amount=10&difficulty=medium
 
-
 if __name__ == "__main__":
     main()
+	
